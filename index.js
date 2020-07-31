c2a8f318919fd9ac85d7ccc836aa490173f09dc4
basil08 <58484487+basil08@users.noreply.github.com>
2020-07-31 17:15:02 +0530

    updated to version 2.
    Added computer-generated scenes. Revised tree structure. Defined new scene objects.

--- index.js
+++ index.js
@@ -1,285 +1,14 @@
 /* 
- * The Game Engine v1.0
+ * The Game Engine v2.0
  */
 
-// 25 is oh, say how you want us to improve(since you are leaving the game midway)
-// 26 is end-credits starts 
-// 27 is end-credits continued
 
-
-
-let s0 = {
-	id:"0",
-	img:"images/scene0.jpg",
-	text:"Start Exploring?",
-	dec:["Yes","No"]
-};
-
-let s1 = {
-	id:"1",
-	img:"images/scene1.jpg",
-	text:"Dr. Sadao Hoki - the protagonist of the story.",
-	dec:["OK"]
-};
-
-let s2 = {
-	id:"2",
-	img:"images/scene2.jpg",
-	text:"Hana - Sadao's wife.",
-	dec:["OK"]
-}
-
-let s3 = {
-	id:"3",
-	img:"images/scene3.jpg",
-	text:"The General - A Japanese WW2 General.",
-	dec:["OK"]
-}
-
-let s4 = {
-	id:"4",
-	img:"images/scene4.jpg",
-	text:"Ready Player One?",
-	dec:["Yes","No"]
-}
-
-let s5 = {
-	id:"5",
-	img:"images/scene5.jpg",
-	text:"Dr. Sadoa Hoki's house was built on the Japanese coast set upon rocks that was outlined by bent pines.",
-	dec:["OK"]
-};
-
-let s6 = {
-	id:"6",
-	img:"images/scene6.jpg",
-	text:"Standing at the veranda, looking at the vast and mighty sea was a habit of Dr. Sadao",
-	dec:["OK"]
-}
-
-let s7 = {
-	id:"7",
-	img:"images/scene7.jpg",
-	text:"His father had taken the little boy to the islands of the sea and said 'Those islands yonder, they are the stepping stones to the future for Japan.'</b>",
-	dec:["OK"]
-}
-
-let s8 = {
-	id:"8",
-	img:"images/scene8.jpg",
-	text:"At this moment, the door opens and Hana came to him affectionately, dressed in a dark-blue haori over her kimono.",
-	dec:["OK"]
-}
-
-let s9 = {
-	id:"9",
-	img:"images/scene9.jpg",
-	text:"It was by chance that they had met and fallen in love in America where Sadao had gone to study medicine.",
-	dec:["OK"]
-}
-
-let s10 = {
-	id:"10",
-	img:"images/scene10.jpg",
-	text:"Suddenly, they spotted a dark figure washed ashore.",
-	dec:["OK"]
-}
-
-let s11 = {
-	id:"11",
-	img:"images/scene11.jpg",
-	text:"To their dismay, the figure was not a Japanese fisherman but an American prisoner of war!",
-	dec:["OK"]
-}
-
-let s12 = {
-	id:"12",
-	img:"images/scene12.jpg",
-	text:"After much conflict, they secretly smuggled him into the house in the dead quiet of the night.",
-	dec:["OK"]
-}
-
-let s13 = {
-	id:"13",
-	img:"images/scene13.jpg",
-	text:"Yumi, the housemaid, refused to touch the man.<br>Hence, Hana, herself, cleaned the helpless youth.",
-	dec:["OK"]
-}
-
-let s14 = {
-	id:"14",
-	img:"images/scene14.jpg",
-	text:"Sadao operated on the man and it was his expertise that ultimately saved the life of the American soldier.",
-	dec:["OK"]
-}
-
-let s15 = {
-	id:"15",
-	img:"images/scene15.jpg",
-	text:"Servants quietly expressed their disapprovals at the behavior of their masters.",
-	dec:["OK"]
-}
-
-let s16 = {
-	id:"16",
-	img:"images/scene16.jpg",
-	text:"Sadao decides to write a letter to the police but somehow, could never finish it.",
-	dec:["OK"]
-}
-
-let s17 = {
-	id:"17",
-	img:"images/scene17.jpg",
-	text:"A messenger arrives with the news of the General's deteriorating health. Sadao is immediately summoned.",
-	dec:["OK"]
-}
-
-let s18 = {
-	id:"18",
-	img:"images/scene18.jpg",
-	text:"Sadao and the General conspire to send assassins to kill the American to let Sadao off the hook.",
-	dec:["OK"]
-}
-
-let s19 = {
-	id:"19",
-	img:"images/scene19.jpg",
-	text:"The first night.",
-	dec:["OK"]
-}
-
-let s20 = {
-	id:"20",
-	img:"images/scene20.jpg",
-	text:"Nothing happened.",
-	dec:["OK"]
-}
-
-let s21 = {
-	id:"21",
-	img:"images/scene21.jpg",
-	text:"Sadao thought,'Maybe it's the second night'.",
-	dec:["OK"]
-}
-
-let s22 = {
-	id:"22",
-	img:"images/scene22.jpg",
-	text:"Nothing happened",
-	dec:["OK"]
-}
-
-let s23 = {
-	id:"23",
-	img:"images/scene23.jpg",
-	text:"Sadao was sure that it had to be the third night.",
-	dec:["OK"]
-}
-
-let s24 = {
-	id:"24",
-	img:"images/scene24.jpg",
-	text:"Nothing happened",
-	dec:["OK"]
-}
-
-let s25 = {
-	id:"25",
-	img:"images/scene25.jpg",
-	text:"Overwhelmed by the strain and responsibilities as a father and husband, he devised a plan to get rid of the soldier himself.",
-	dec:["OK"]
-}
-
-let s26 = {
-	id:"26",
-	img:"images/scene26.jpg",
-	text:"Sadao bids the man goodbye",
-	dec:["OK"]
-}
-
-let s27 = {
-	id:"27",
-	img:"images/scene27.jpg",
-	text:"The General had forgotten to keep his promise",
-	dec:["OK"]
-}
-
-let s28 = {
-	id:"28",
-	img:"images/scene28.jpg",
-	text:"Everything turns back to normal and the soldier was saved",
-	dec:["OK"]
-}
-
-let s29 = {
-	id:"29",
-	img:"images/scene29.jpg",						// IMAGE S25 is THE END
-	text:"THE END.",
-	dec:["OK"]
-}
-
-
-let s30 = {
-	id:"30",
-	img:"images/scene30.jpg",
-	text:"Sorry to see you leave! You may tell us how you felt and what we can do better at abc@email.com",
-	dec:["Sure"]
-}
-
-let s31 = {
-	id:"31",
-	img:"images/scene31.jpg",						// IMAGE SCENE 26 is CREDITS 1
-	text:"",
-	dec:["OK"]
-}
-
-let s32 = {
-	id:"32",
-	img:"images/scene32.jpg",						// IMAGE SCENE 27 is CREDITS 2
-	text:"Start Over?",
-	dec:["Yes"]
-}
-
-
-let tree = {
-	"0": [s1, s30],
-	"1": [s2],
-	"2": [s3],
-	"3": [s4],
-	"4": [s5, s30],
-	"5": [s6],
-	"6": [s7],
-	"7": [s8],
-	"8": [s9],
-	"9": [s10],
-	"10":[s11],
-	"11":[s12],
-	"12":[s13],
-	"13":[s14],
-	"14":[s15],
-	"15":[s16],
-	"16":[s17],
-	"17":[s18],
-	"18":[s19],
-	"19":[s20],
-	"20":[s21],
-	"21":[s22],
-	"22":[s23],
-	"23":[s24],
-	"24":[s25],
-	"25":[s26],
-	"26":[s27],
-	"27":[s28],
-	"26":[s29],
-	"29":[s31],
-	"30":[s31],
-	"31":[s32],
-	"32":[s5]
-};
-
-
-
-
+// TODO: Crop pictures to perfect size..some are bit offset(courtesy, my sister)
+// TODO: Add Description Text to narrate the story 
+// TODO: Make Description Text prominent (use CSS)
+// TODO: Change Button styling. Current one looks awkward
+// TODO: Add Background color (different for adapted story and different for main branch)
+// TODO: 
 
 let currentScene;
 
@@ -293,10 +22,9 @@ function updateScene(decision){
 	updateDOM();
 } 
   	
-function buttonHandler(){
+function buttonHandler(event){
 	let decision = event.srcElement.textContent;
 	updateScene(decision);
-
 }
 
  function init(){
@@ -306,7 +34,7 @@ function buttonHandler(){
 
  	document.getElementById("decision1").onclick = buttonHandler;
  	document.getElementById("decision2").onclick = buttonHandler;
- 	// updateScene();
+
  	updateDOM();
  }
 
--- scenes.js
+++ scenes.js
@@ -1,94 +1,494 @@
-// 25 is oh, say how you want us to improve(since you are leaving the game midway)
-// 26 is end-credits starts 
-// 27 is end-credits continued
-
 
 
 let s0 = {
-	id:"0",
-	img:"images/scene0.jpg",
-	text:"Start Exploring?",
-	dec:["Yes","No"]
-};
+	id:'0',
+	img:'images/s0.jpg',
+	text:'',
+	dec:['OK']
+}
 
 let s1 = {
-	id:"1",
-	img:"images/featuring-sadao.jpg",
-	text:"Dr. Sadao Hoki - the protagonist of the story.",
-	dec:["Ok"]
-};
+	id:'1',
+	img:'images/s1.jpg',
+	text:'',
+	dec:['OK']
+}
 
 let s2 = {
-	id:"2",
-	img:"images/featuring-hana.jpg",
-	text:"Hana - Sadao's wife.",
-	dec:["Ok"]
+	id:'2',
+	img:'images/s2.jpg',
+	text:'Ready Player One?',
+	dec:['Yes','No']
 }
 
 let s3 = {
-	id:"3",
-	img:"images/featuring-general.jpg",
-	text:"The General - A Japanese WW2 General.",
-	dec:["Ok"]
+	id:'3',
+	img:'images/s3.jpg',
+	text:'Proceed!',
+	dec:['OK']
 }
 
 let s4 = {
-	id:"4",
-	img:"featuring-servants.jpg",
-	text:"Ready Player One?",
-	dec:["Yes","No"]
+	id:'4',
+	img:'images/s4.jpg',
+	text:'',
+	dec:['OK']
 }
 
 let s5 = {
-	id:"5",
-	img:"images/scene0.jpg",
-	text:"Introduction to Sadao's house",
-	dec:["Ok"]
-};
+	id:'5',
+	img:'images/s5.jpg',
+	text:'',
+	dec:['OK']
+}
 
 let s6 = {
-	id:"6",
-	img:"images/scene1.jpg",
-	text:"",
-	dec:["Ok"]
+	id:'6',
+	img:'images/s6.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s7 = {
+	id:'7',
+	img:'images/s7.jpg',
+	text:'Do you want to know more about Sadao\'s relationship with his father?',
+	dec:['Yes','No']
+}
+
+let s8 = {
+	id:'8',
+	img:'images/s8.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s9 = {
+	id:'9',
+	img:'images/s9.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s10 = {
+	id:'10',
+	img:'images/s10.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s11 = {
+	id:'11',
+	img:'images/s11.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s12 = {
+	id:'12',
+	img:'images/s12.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s13 = {
+	id:'13',
+	img:'images/s13.jpg',
+	text:'You are Sadao! Do you want to carry the American into your house or leave him on the shore to die?',
+	dec:['Carry into House','Leave on the shore']
+}
+
+let s14 = {
+	id:'14',
+	img:'images/s14.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s15 = {
+	id:'15',
+	img:'images/s15.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s16 = {
+	id:'16',
+	img:'images/s16.jpg',
+	text:'Place yourself in Sadao\'s shoes. Do you save the American or kill him?',
+	dec:['Save the American','Kill the American']
+}
+
+let s17 = {
+	id:'17',
+	img:'images/s17.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s18 = {
+	id:'18',
+	img:'images/s18.jpg',
+	text:'Do you want Sadao to complete his letter?',
+	dec:['Yes','No']
+}
+
+let s19 = {
+	id:'19',
+	img:'images/s19.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s20 = {
+	id:'20',
+	img:'images/s20.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s21 = {
+	id:'21',
+	img:'images/s21.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s22 = {
+	id:'22',
+	img:'images/s22.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s23 = {
+	id:'23',
+	img:'images/s23.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s24 = {
+	id:'24',
+	img:'images/s24.jpg',
+	text:'Do you want Tom to escape?',
+	dec:['Yes','No']
+}
+
+let s25 = {
+	id:'25',
+	img:'images/s25.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s26 = {
+	id:'26',
+	img:'images/s26.jpg',
+	text:'You are Tom! Do you break down mentally?',
+	dec:['Yes','No']
 }
 
 let s27 = {
-	id:"27",
-	img:"",
-	text:"Start Over?",
-	dec:["Yes"]
+	id:'27',
+	img:'images/s27.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s28 = {
+	id:'28',
+	img:'images/s28.jpg',
+	text:'Do you want Sadao to be careless?',
+	dec:['Yes','No']
+}
+
+let s29 = {
+	id:'29',
+	img:'images/s29.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s30 = {
+	id:'30',
+	img:'images/s30.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s31 = {
+	id:'31',
+	img:'images/s31.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s32 = {
+	id:'32',
+	img:'images/s32.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s33 = {
+	id:'33',
+	img:'images/s33.jpg',
+	text:'',
+	dec:['OK']
 }
 
 
-let tree = {
-	s0: [s1, s25],
-	s1: [s2],
-	s2: [s3],
-	s3: [s4],
-	s4: [s5, s25],
-	s5: [s6],
-	s6: [s7],
-	s8: [s9],
-	s9: [s10],
-	s10:[s11],
-	s11:[s12],
-	s12:[s13],
-	s13:[s14],
-	s14:[s15],
-	s15:[s16],
-	s16:[s17],
-	s17:[s18],
-	s18:[s19],
-	s19:[s20],
-	s20:[s21],
-	s21:[s22],
-	s22:[s23],
-	s23:[s24],
-	s24:[s26],
-	s25:[s26],
-	s26:[s27],
-	s27:[s5]			// start over?
-};
+let s34 = {
+	id:'34',
+	img:'images/s34.jpg',
+	text:'',
+	dec:['OK']
+}
 
+let s35 = {
+	id:'35',
+	img:'images/s35.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s36 = {
+	id:'36',
+	img:'images/s36.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s37 = {
+	id:'37',
+	img:'images/s37.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s38 = {
+	id:'38',
+	img:'images/s38.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s39 = {
+	id:'39',
+	img:'images/s39.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s40 = {
+	id:'40',
+	img:'images/s40.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s41 = {
+	id:'41',
+	img:'images/s41.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s42 = {
+	id:'42',
+	img:'images/s42.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s43 = {
+	id:'43',
+	img:'images/s43.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s44 = {
+	id:'44',
+	img:'images/s44.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s45 = {
+	id:'45',
+	img:'images/s45.jpg',
+	text:'You are the messenger. Do you betray Sadao?',
+	dec:['Yes','No']
+}
+
+let s46 = {
+	id:'46',
+	img:'images/s46.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s47 = {
+	id:'47',
+	img:'images/s47.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s48 = {
+	id:'48',
+	img:'images/s48.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s49 = {
+	id:'49',
+	img:'images/s49.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s50 = {
+	id:'50',
+	img:'images/s50.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s51 = {
+	id:'51',
+	img:'images/s51.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s52 = {
+	id:'52',
+	img:'images/s52.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s53 = {
+	id:'53',
+	img:'images/s53.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s54 = {
+	id:'54',
+	img:'images/s54.jpg',
+	text:'Do you want Tom to board a Japanese Navy vessel?',
+	dec:['Yes','No']
+}
+
+let s55 = {
+	id:'55',
+	img:'images/s55.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s56 = {
+	id:'56',
+	img:'images/s56.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s57 = {
+	id:'57',
+	img:'images/s57.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s58 = {
+	id:'58',
+	img:'images/s58.jpg',
+	text:'',
+	dec:['OK']
+}
+
+let s59 = {
+	id:'59',
+	img:'images/s59.jpg',
+	text:'Wanna Start Over?',
+	dec:['Of course!']
+}
+
+let s60 = {
+	id:'60',
+	img:'images/s60.jpg',
+	text:'Wanna Start Over?',
+	dec:['Of course!']
+}
+
+let tree = {
+	"0": [s1],
+	"1": [s2],
+	"2": [s3, s60],
+	"3": [s4],
+	"4": [s5],
+	"5": [s6],
+	"6": [s7],
+	"7": [s8, s10],
+	"8": [s9],
+	"9": [s10],
+	"10":[s11],
+	"11":[s12],
+	"12":[s13],
+	"13":[s14, s36],
+	"14":[s15],
+	"15":[s16],
+	"16":[s17, s38],
+	"17":[s18],
+	"18":[s44, s19],
+	"19":[s20],
+	"20":[s21],
+	"21":[s22],
+	"22":[s23],
+	"23":[s24],
+	"24":[s50,s25],
+	"25":[s26],
+	"26":[s53, s27],
+	"27":[s28],
+	"28":[s57,s29],
+	"29":[s30],
+	"30":[s31],
+	"31":[s32],
+	"32":[s33],
+	"33":[s34],
+	"34":[s35],
+	"35":[s59],
+	"36":[s37],
+	"37":[s33],
+	"38":[s39],
+	"39":[s40],
+	"40":[s33],
+	"41":[s42],
+	"42":[s43],
+	"43":[s33],
+	"44":[s45],
+	"45":[s46, s47],
+	"46":[s42],
+	"47":[s48],
+	"48":[s49],
+	"49":[s33],
+	"50":[s51],
+	"51":[s52],
+	"52":[s33],
+	"53":[s54],
+	"54":[s55],
+	"55":[s52],
+	"56":[s33],
+	"57":[s58],
+	"58":[s42],
+	"59":[s0],				// start over? 
+	"60":[s0],
+};
 

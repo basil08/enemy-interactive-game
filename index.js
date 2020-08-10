69297baf1d436ba202a1a94ba4d3c62e7a7f2653
basil08 <58484487+basil08@users.noreply.github.com>
2020-08-10 09:19:22 +0530

    Upgraded to version 3
    Added new scene descriptions. A background image and new color palette(blue).

--- index.js
+++ index.js
@@ -8,11 +8,11 @@
 // TODO: Make Description Text prominent (use CSS)
 // TODO: Change Button styling. Current one looks awkward
 // TODO: Add Background color (different for adapted story and different for main branch)
-// TODO: 
+// TODO: Poistion Button properly in a div. This one gets off-set weirdly.
 
 let currentScene;
 
-function updateScene(decision){
+function updateScene(decision) {
 	let possible = tree[currentScene.id];
 	let chosen = currentScene.dec.indexOf(decision);
 
@@ -20,41 +20,41 @@ function updateScene(decision){
 	currentScene = possible[chosen];
 
 	updateDOM();
-} 
-  	
-function buttonHandler(event){
+}
+
+function buttonHandler(event) {
 	let decision = event.srcElement.textContent;
 	updateScene(decision);
 }
 
- function init(){
- 	// make and initialize the game object 
- 	// attach event handlers to each button 
- 	currentScene = s0;
+function init() {
+	// make and initialize the game object 
+	// attach event handlers to each button 
+	currentScene = s0;
 
- 	document.getElementById("decision1").onclick = buttonHandler;
- 	document.getElementById("decision2").onclick = buttonHandler;
+	document.getElementById("decision1").onclick = buttonHandler;
+	document.getElementById("decision2").onclick = buttonHandler;
 
- 	updateDOM();
- }
+	updateDOM();
+}
 
 
-function updateDOM(){
+function updateDOM() {
 
 	document.getElementById("sceneimg").src = currentScene.img;
 	document.getElementById("scenetext").innerHTML = currentScene.text;
 
-	if(currentScene.dec[0]){
+	if (currentScene.dec[0]) {
 		document.getElementById("decision1").textContent = currentScene.dec[0];
 		document.getElementById("decision1").style.visibility = "visible";
-	}else{
+	} else {
 		document.getElementById("decision1").style.visibility = "hidden";
 	}
 
-	if(currentScene.dec[1]){
+	if (currentScene.dec[1]) {
 		document.getElementById("decision2").textContent = currentScene.dec[1];
 		document.getElementById("decision2").style.visibility = "visible";
-	}else{
+	} else {
 		document.getElementById("decision2").style.visibility = "hidden";
 	}
 }
--- scenes.js
+++ scenes.js
@@ -45,7 +45,7 @@ let s5 = {
 let s6 = {
 	id:'6',
 	img:'images/s6.jpg',
-	text:'',
+	text:'Sadao was raised by his father, an able fisherman, who ensured his son had a good education.',
 	dec:['OK']
 }
 
@@ -59,175 +59,175 @@ let s7 = {
 let s8 = {
 	id:'8',
 	img:'images/s8.jpg',
-	text:'',
-	dec:['OK']
+	text:'Sadao\'s father like all his countrymen was patriotic. He passed this trait to his son.',
+	dec:['I want to know more!']
 }
 
 let s9 = {
 	id:'9',
 	img:'images/s9.jpg',
-	text:'',
-	dec:['OK']
+	text:'The seeds of Sadao\'s thirst for knowledge and curiosity were sown by his father. Sadao went on to be a scientist.',
+	dec:['Interesting to know!']
 }
 
 let s10 = {
 	id:'10',
 	img:'images/s10.jpg',
-	text:'',
+	text:'Hana was a loving and responsible wife always giving in to Sadao\'s needs and following the Japanese way of life.',
 	dec:['OK']
 }
 
 let s11 = {
 	id:'11',
 	img:'images/s11.jpg',
-	text:'',
-	dec:['OK']
+	text:'It was love at first sight for Sadao and Hana.',
+	dec:['What happened next?']
 }
 
 let s12 = {
 	id:'12',
 	img:'images/s12.jpg',
-	text:'',
+	text:'The couple found a helpless and wounded white man on the shore. The place was covered by mist just like their future. They knew the repurcussions of being sighted with the "Enemy"',
 	dec:['OK']
 }
 
 let s13 = {
 	id:'13',
 	img:'images/s13.jpg',
-	text:'You are Sadao! Do you want to carry the American into your house or leave him on the shore to die?',
+	text:'Thinking about their children, their great Empire and their foremost duty to save life, they hesitated to make a decision. Towards morality or humanity?<br>You are Sadao! Do you want to carry the American into your house or leave him on the shore to die?',
 	dec:['Carry into House','Leave on the shore']
 }
 
 let s14 = {
 	id:'14',
 	img:'images/s14.jpg',
-	text:'',
+	text:'They could not abide by fear. Saving a life was the most important to them. Stealthily they made their way with the unconcious body into their house.',
 	dec:['OK']
 }
 
 let s15 = {
 	id:'15',
 	img:'images/s15.jpg',
-	text:'',
+	text:'Not all people had their mindset. Fear controlled even their own servants. The mistress was left to tend the man alone.',
 	dec:['OK']
 }
 
 let s16 = {
 	id:'16',
 	img:'images/s16.jpg',
-	text:'Place yourself in Sadao\'s shoes. Do you save the American or kill him?',
+	text:'Yet again, Sadao faced the same cringing dilemma . To choose between his dear one\'s protection or to act according to his foremost duty to help people fight death!<br>Place yourself in Sadao\'s shoes! Do you save the American or kill him?',
 	dec:['Save the American','Kill the American']
 }
 
 let s17 = {
 	id:'17',
 	img:'images/s17.jpg',
-	text:'',
-	dec:['OK']
+	text:'Terrified and disgusted by their master\'s actions, the forever loyal servants discussed the matter among themselves!<br>You are the anxious cook! Do you betray your master?',
+	dec:['Yes','No']
 }
 
 let s18 = {
 	id:'18',
 	img:'images/s18.jpg',
-	text:'Do you want Sadao to complete his letter?',
+	text:'Remembering his oath to serve his country especially during times of hardship he started to write a letter to the General.<br>Do you want Sadao to complete his letter?',
 	dec:['Yes','No']
 }
 
 let s19 = {
 	id:'19',
 	img:'images/s19.jpg',
-	text:'',
-	dec:['OK']
+	text:'Hana almost exposed their fatal secret!',
+	dec:['Oh!']
 }
 
 let s20 = {
 	id:'20',
 	img:'images/s20.jpg',
-	text:'',
+	text:'Both Sadao and General knew what had to be done. For sadao it was the removal of a major threat. For the General, it was a matter of saving his life as no other person could treat him like Sadao.',
 	dec:['OK']
 }
 
 let s21 = {
 	id:'21',
 	img:'images/s21.jpg',
-	text:'',
+	text:'To protect his pride, integrity and blind devotion towards his nation he devises a plan to "uproot the weed"!',
 	dec:['OK']
 }
 
 let s22 = {
 	id:'22',
 	img:'images/s22.jpg',
-	text:'',
-	dec:['OK']
+	text:'Tom could not help but thank Sadao for saving his life and giving him hope!',
+	dec:['Woah!']
 }
 
 let s23 = {
 	id:'23',
 	img:'images/s23.jpg',
-	text:'',
-	dec:['OK']
+	text:'Naturally, anxiety bursts through the veins of the Japanese couple. They felt chained in the circumstance!',
+	dec:['What happened next?']
 }
 
 let s24 = {
 	id:'24',
 	img:'images/s24.jpg',
-	text:'Do you want Tom to escape?',
+	text:'Tom gets better mentally and physically. The fear of being executed shivers down his spine!<br><b>Do you want Tom to escape?</b>',
 	dec:['Yes','No']
 }
 
 let s25 = {
 	id:'25',
 	img:'images/s25.jpg',
-	text:'',
+	text:'Sadao thinks of the assassins and his beloved!',
 	dec:['OK']
 }
 
 let s26 = {
 	id:'26',
 	img:'images/s26.jpg',
-	text:'You are Tom! Do you break down mentally?',
+	text:'Now with all his strength Tom faces conflicting opinions. To forcefully break free of this unstable and risky alliance or to be grateful towards his saviour.<br><b>You are Tom! Do you break down mentally?</b>',
 	dec:['Yes','No']
 }
 
 let s27 = {
 	id:'27',
 	img:'images/s27.jpg',
-	text:'',
-	dec:['OK']
+	text:'Sadao devises a plan to help Tom escape!',
+	dec:['Then what happened?']
 }
 
 let s28 = {
 	id:'28',
 	img:'images/s28.jpg',
-	text:'Do you want Sadao to be careless?',
+	text:'Tom is no longer Sadao\'s responsibility. He can do whatever pleases him. <br><b>Do you want Sadao to be careless?</b>',
 	dec:['Yes','No']
 }
 
 let s29 = {
 	id:'29',
 	img:'images/s29.jpg',
-	text:'',
+	text:'Sadao is intruiged by the thought that the General did not execute the "enemy". The self-centred general did not think about anyone but himself!',
 	dec:['OK']
 }
 
 let s30 = {
 	id:'30',
 	img:'images/s30.jpg',
-	text:'',
+	text:'To protect himself and his trusted doctor he keeps it a secret!',
 	dec:['OK']
 }
 
 let s31 = {
 	id:'31',
 	img:'images/s31.jpg',
-	text:'',
+	text:'In the end, humanity wins. The servants return after they are assured there is no more danger in attending to their master\'s needs!',
 	dec:['OK']
 }
 
 let s32 = {
 	id:'32',
 	img:'images/s32.jpg',
-	text:'',
+	text:'After the chilling experience, Sadao recounts all the dreadful and beautiful moments in his life, most of them centred around America. He searches for the specific memory which stopped him from killing the "ENEMY"!',
 	dec:['OK']
 }
 
@@ -256,105 +256,105 @@ let s35 = {
 let s36 = {
 	id:'36',
 	img:'images/s36.jpg',
-	text:'',
+	text:'Sadao crumbles under the intense pressure. The couple agree to do what is best for themselves!',
 	dec:['OK']
 }
 
 let s37 = {
 	id:'37',
 	img:'images/s37.jpg',
-	text:'',
+	text:'The ocean roared. No sign of life on the beach, no dying breath!',
 	dec:['OK']
 }
 
 let s38 = {
 	id:'38',
 	img:'images/s38.jpg',
-	text:'',
+	text:'Sadao stops by thinking about his children. The operation is "successful" but the patient is dead!',
 	dec:['OK']
 }
 
 let s39 = {
 	id:'39',
 	img:'images/s39.jpg',
-	text:'',
+	text:'The sight of the "Enemy" wether alive or dead would not be tolerated by the authorities. The mistress orders her sevant to do what\'s necessary.',
 	dec:['OK']
 }
 
 let s40 = {
 	id:'40',
 	img:'images/s40.jpg',
-	text:'',
+	text:'Servants dispose the motionless body.The ocean roared. No sign of life on the beach!',
 	dec:['OK']
 }
 
 let s41 = {
 	id:'41',
 	img:'images/s41.jpg',
-	text:'',
+	text:'Personal safety and national integrity was more important to the cook!',
 	dec:['OK']
 }
 
 let s42 = {
 	id:'42',
 	img:'images/s42.jpg',
-	text:'',
+	text:'It\'s a gruesome ending for Tom!',
 	dec:['OK']
 }
 
 let s43 = {
 	id:'43',
 	img:'images/s43.jpg',
-	text:'',
+	text:'Sadao can\'t help but accept his fate. The servants are never seen again!',
 	dec:['OK']
 }
 
 let s44 = {
 	id:'44',
 	img:'images/s44.jpg',
-	text:'',
+	text:'Thinking of his family and proud that at least he has saved Tom\'s life, he completes the letter!',
 	dec:['OK']
 }
 
 let s45 = {
 	id:'45',
 	img:'images/s45.jpg',
-	text:'You are the messenger. Do you betray Sadao?',
+	text:'The messenger smells suspicion in the air. He can choose to be loyal to Sadao or his rulers!<br><b>You are the messenger. Do you betray Sadao?</b>',
 	dec:['Yes','No']
 }
 
 let s46 = {
 	id:'46',
 	img:'images/s46.jpg',
-	text:'',
-	dec:['OK']
+	text:'The fear of execution and the morality towards his country mattered most to the messenger. The official now opens the letter!',
+	dec:['And...?']
 }
 
 let s47 = {
 	id:'47',
 	img:'images/s47.jpg',
-	text:'',
+	text:'The General thinks about Sadao\'s safety as his life depends on him.',
 	dec:['OK']
 }
 
 let s48 = {
 	id:'48',
 	img:'images/s48.jpg',
-	text:'',
+	text:'The general is driven by the urge to "save" his country, he plans a secret mission!',
 	dec:['OK']
 }
 
 let s49 = {
 	id:'49',
 	img:'images/s49.jpg',
-	text:'',
+	text:'The dissapearance of the undesirable visitor raises alarms. Only Sadao comes close enough to unveil what may have happened to the white American!',
 	dec:['OK']
 }
 
 let s50 = {
 	id:'50',
 	img:'images/s50.jpg',
-	text:'',
+	text:'Fearing execution he sneaks off Sadao\'s house, hoping to spot a life-saving boat!',
 	dec:['OK']
 }
 
@@ -362,55 +362,55 @@ let s51 = {
 	id:'51',
 	img:'images/s51.jpg',
 	text:'',
-	dec:['OK']
+	dec:['And then what happened?']
 }
 
 let s52 = {
 	id:'52',
 	img:'images/s52.jpg',
-	text:'',
+	text:'It\'s a gruesome ending for Tom!',
 	dec:['OK']
 }
 
 let s53 = {
 	id:'53',
 	img:'images/s53.jpg',
-	text:'',
+	text:'Driven by immense stress and anxiety, Tom breaks down and is desperate to break free. He senses an oppurtunity to take advantage of his host!',
 	dec:['OK']
 }
 
 let s54 = {
 	id:'54',
 	img:'images/s54.jpg',
-	text:'Do you want Tom to board a Japanese Navy vessel?',
+	text:'In the heat of the moment, Sadao does not mention crucial details about the Korean boats!<br><b>Do you want Tom to board a Japanese Navy vessel?</b>',
 	dec:['Yes','No']
 }
 
 let s55 = {
 	id:'55',
 	img:'images/s55.jpg',
-	text:'',
+	text:'The unsuspecting Tom signals a Japanese boat and steps towards his own doom!',
 	dec:['OK']
 }
 
 let s56 = {
 	id:'56',
 	img:'images/s56.jpg',
-	text:'',
+	text:'Luck was on the American\'s side. He will always remember the Japanese family. He qestions his spiteful way of treating them.',
 	dec:['OK']
 }
 
 let s57 = {
 	id:'57',
 	img:'images/s57.jpg',
-	text:'',
+	text:'A fisherman is suprised to see Sadao, who never ventures out of his home at night talking alone with a military man in the dark!',
 	dec:['OK']
 }
 
 let s58 = {
 	id:'58',
 	img:'images/s58.jpg',
-	text:'',
+	text:'The General cannot believe his eyes. Never in his dreams he could think that great Sadao could be a traitor. It has become his duty to safeguard the Japanese empire!',
 	dec:['OK']
 }
 
@@ -446,7 +446,7 @@ let tree = {
 	"14":[s15],
 	"15":[s16],
 	"16":[s17, s38],
-	"17":[s18],
+	"17":[s41, s18],
 	"18":[s44, s19],
 	"19":[s20],
 	"20":[s21],
